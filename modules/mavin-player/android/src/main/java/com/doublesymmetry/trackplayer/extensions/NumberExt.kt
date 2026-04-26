deeed7c100d4394d7461fedfb910d2597468dfa0
Phoenix-Boss <bossblingzs@gmail.com>
2026-04-26 16:47:25 +0100

    headless

--- modules/mavin-player/android/src/main/java/com/doublesymmetry/trackplayer/extensions/NumberExt.kt
+++ modules/mavin-player/android/src/main/java/com/doublesymmetry/trackplayer/extensions/NumberExt.kt
@@ -1,9 +1,13 @@
-﻿package com.doublesymmetry.trackplayer.extensions
+package com.doublesymmetry.trackplayer.extensions
 
 class NumberExt {
     companion object {
-        fun Double.toMilliseconds(): Long = (this * 1000).toLong()
-        fun Long.toSeconds(): Double = this / 1000.0
-        fun Double.toSeconds(): Double = this / 1000.0
+        fun Number.toSeconds(): Double {
+            return this.toDouble() / 1000
+        }
+
+        fun Number.toMilliseconds(): Long {
+            return (this.toDouble() * 1000).toLong()
+        }
     }
 }
