7801ff81b54d4f1e5dd98a59edb1569c6580d34f
Phoenix-Boss <bossblingzs@gmail.com>
2026-05-01 01:30:30 +0100

    enable TurboModule and New Architecture for mavin-player

--- app.config.js
+++ app.config.js
@@ -19,7 +19,7 @@ module.exports = {
   icon: "./assets/images/icon.png",
   scheme: IS_DEV ? "mavins-player-dev" : "mavins-player",
   userInterfaceStyle: "automatic",
-  newArchEnabled: false,
+  newArchEnabled: true,
   android: {
     softwareKeyboardLayoutMode: "pan",
     permissions: [
