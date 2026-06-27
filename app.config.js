ffb5955e55b651052c3e407d03c6e8dd1eedf5e2
Phoenix-Boss <bossblingzs@gmail.com>
2026-06-27 09:46:02 +0100

    Use EAS secret for google-services.json

--- app.config.js
+++ app.config.js
@@ -53,7 +53,7 @@ module.exports = {
     backgroundColor: "#000",
     edgeToEdgeEnabled: true,
     versionCode: 1,
-    googleServicesFile: "./google-services.json",
+    googleServicesFile: process.env.GOOGLE_SERVICES_JSON ?? "./google-services.json",
     intentFilters: [
       {
         action: "android.intent.action.MAIN",
